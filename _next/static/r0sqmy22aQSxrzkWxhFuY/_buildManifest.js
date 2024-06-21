5f36d71339c4658c71ddf925f46e934df37e1670
rotemlevi <rotem85.levi@gmail.com>
2024-06-22 00:01:17 +0300

    Updates

--- _next/static/r0sqmy22aQSxrzkWxhFuY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/843-3b4979b1e15a340e.js","static/chunks/pages/index-d3a34a3db7c0ff59.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
