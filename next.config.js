024e56b124159d2cb1598dc740270fe54d54a5b0
rotemlevi <rotem85.levi@gmail.com>
2024-06-21 23:29:10 +0300

    add deployment to github

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  assetPrefix: isProd ? '/my-tests/' : '',
+  basePath: isProd ? '/my-tests' : '',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  output: 'export',
+}
